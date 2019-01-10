441ac9dbad9205d90302ea5302c12471592eab78
chenjinfu <chenjf@yunjiglobal.com>
2019-01-10 14:35:02 +0800

    提交LRU2

---
+++ src/com/hin/bridge/bridge/LRU2.java
@@ -0,0 +1,11 @@
+package com.hin.bridge.bridge;
+
+/**
+ * @author chenjinfu
+ * @since 2019/01/10
+ */
+public class LRU2 {
+    public static void main(String[] args) {
+        System.out.println("=====================");
+    }
+}
